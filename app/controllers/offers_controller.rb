2fa5a45a2ff11312cef5e257450138ce550e58ae
Danilo Castro <danilogcastro@hotmail.com>
2021-05-31 17:00:47 -0300

    implemented index, show, new and create in Offers controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,31 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    raise
+    @offer = Offer.new(offer_params)
+    @user = User.find(params[:user_id])
+    @offer.user = @user
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:instrument, :price, :location)
+  end
 end
