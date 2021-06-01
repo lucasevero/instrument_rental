ec625341c28b6e30e7ade164748bcac1ca9df538
Danilo Castro <danilogcastro@hotmail.com>
2021-06-01 12:51:42 -0300

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_193330) do
+ActiveRecord::Schema.define(version: 2021_05_31_212530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.string "status", default: "pending"
+    t.bigint "offer_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["offer_id"], name: "index_deals_on_offer_id"
+    t.index ["user_id"], name: "index_deals_on_user_id"
+  end
 
   create_table "offers", force: :cascade do |t|
     t.string "instrument"
@@ -37,5 +47,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_193330) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "deals", "offers"
+  add_foreign_key "deals", "users"
   add_foreign_key "offers", "users"
 end
