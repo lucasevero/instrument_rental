4597ba059df7b962e72de1f5dc8841da7250ca6e
Danilo Castro <danilogcastro@hotmail.com>
2021-06-01 14:34:37 -0300

    wrote some front end for offers JUST FOR TESTING PURPOSES//ERASE LATER

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,6 @@
 class Deal < ApplicationRecord
   belongs_to :offer
   belongs_to :user
+
+  validates :start_date, :end_date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_212530) do
+ActiveRecord::Schema.define(version: 2021_06_01_171424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_05_31_212530) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["offer_id"], name: "index_deals_on_offer_id"
     t.index ["user_id"], name: "index_deals_on_user_id"
   end
