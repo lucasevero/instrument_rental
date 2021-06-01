bc674606fec2b5dc14df79fe4b62ec4a84358946
Danilo Castro <danilogcastro@hotmail.com>
2021-06-01 17:32:13 -0300

    added picture association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :deals
   has_many :offers
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
