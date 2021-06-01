4075a70ddbcd09dfe939b6a23024c6858d6440bf
Carla Valdivia <valdivia.carla@gmail.com>
2021-06-01 15:49:03 -0300

    Fixed deals controller and tested the routes

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,10 @@
 class Deal < ApplicationRecord
+
+  STATUS = ['pending', 'approved', 'denied']
   belongs_to :offer
   belongs_to :user
 
   validates :start_date, :end_date, presence: true
+
+  validates :status, inclusion: { in: STATUS }
 end
