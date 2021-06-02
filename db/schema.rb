89b539d1d82d33b86d4f1f058b8e5e619999807d
Andre Menezes <djdedemenezes@gmail.com>
2021-06-02 11:23:32 -0300

    added available to Offers and defined the default as pending. Only options are pending, approved and denied

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_01_213333) do
+ActiveRecord::Schema.define(version: 2021_06_02_141339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_213333) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.string "picture_url"
+    t.string "available", default: "pending"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
