263a2a9079a7306860cef3dd72cfe1b59312c7db
Andre Menezes <djdedemenezes@gmail.com>
2021-06-03 12:48:23 -0300

    We should use scope on policy when checkiong for more than one controller at the same time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  
+  # Scope é util quando usados em varios controllers ao mesmo tempo
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
