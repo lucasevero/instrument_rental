8047facf0a7f1e54c37c3cde1cfacef80fc3b11d
Andre Menezes <djdedemenezes@gmail.com>
2021-06-03 13:08:02 -0300

    Every controller action need to be authorize on the model_policy. There you decide if it's open to all users (return true) or specific user (record.user == user)

--- app/policies/offer_policy.rb
+++ app/policies/offer_policy.rb
@@ -5,8 +5,25 @@ class OfferPolicy < ApplicationPolicy
     end
   end
 
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+
   def create?
     return true
   end
-  
+
+  def show?
+    return true
+  end
+
 end
